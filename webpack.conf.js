b49b58727dc9f04804672abb30c26eedb2ca4564
Ada Void <oxiggy@oxiggy.com>
2019-07-15 13:02:25 +0200

    сборка и пример

---
+++ webpack.conf.js
@@ -0,0 +1,80 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const path = require('path');
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.jade$/,
+                loaders: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            attrs: false,
+                        },
+                    },
+                    {
+                        loader: 'pug-html-loader',
+                        options: {
+                            data: {
+                                date: '15.07.2019',
+                            },
+                            pretty: true
+                        }
+                    },
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader',
+                    {
+                        loader: 'less-loader',
+                        options: {
+                            javascriptEnabled: true
+                        }
+                    },
+                ],
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'images',
+                        },
+                    },
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.jade',
+            inject: true,
+            filename: 'index.html',
+        }),
+        new CopyWebpackPlugin([
+            { from: 'src/public' },
+        ]),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
+    ],
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
